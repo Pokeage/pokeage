475310eef04edbf78bb14483e34f7b00b16afa8e
Pokeage <257376032+Pokeage@users.noreply.github.com>
2026-04-16 23:35:42 +0900

    feat: implement mod in programs pokeage src state

---
+++ programs/pokeage/src/state/mod.rs
@@ -0,0 +1,13 @@
+//! state account modules.
+
+pub mod card;
+pub mod config;
+pub mod listing;
+pub mod player;
+pub mod pool;
+
+pub use card::*;
+pub use config::*;
+pub use listing::*;
+pub use player::*;
+pub use pool::*;
